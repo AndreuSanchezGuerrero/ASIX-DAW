080f32f778a8fa77c44619a92349cd3a8363cc8c
Niluxis311 <nil.masso.caba@gmail.com>
2022-03-19 15:43:04 +0100

    .

--- Programació/UF2/Practiques/PR2/Corona.py
+++ Programació/UF2/Practiques/PR2/Corona.py
@@ -1,3 +1,4 @@
+#el codi autoincrmen tal coincideix amb les altres variables codi, sh'ha de canviar
 from datetime import *
 import re
 from pacient import *
@@ -19,41 +20,82 @@ def clear():
     else:
         _ = system('clear')
 
+def cap():
+    print(f"Codi | Data Naixement | Sexe | Estat | Inici Contagi | Fi Contagi")
+    print("-"*65)
+
+def printda(p):
+    espai = (" "*(5-len(str((p.codi)))))
+    print(f"{p.codi}{espai}-   {p.dataNaixement}   -  {p.sexe}   -  {p.estat} -  {p.iniciContagi}   -   {p.fiContagi}")
+
 #Metodes Pacients
-def nouPacient():    
+def nouPacient(codi):    
     #Demanarà per teclat les dades del nou pacient
-    codi = codi+1
     p = pacient(codi)
-    dataStr = input("Data Naixement: ")
-    p.dataNaixement = datetime.strptime(dataStr,'%d/%m/%Y')
-    p.sexe = input("Sexe: ")
-    p.estat = input("Estat : ")
-    iniciStr = input("Inici Contagi: ")
-    p.iniciContagi = datetime.strptime(dataStr,'%d/%m/%Y')
-    return p
+    dataStr = input("Data Naixement (diamesany): ")
+    p.dataNaixement = datetime.strptime(dataStr,'%d%m%Y').date()
+    sexe = int(input("Sexe (0=H 1=D): "))
+    p.sexe = sexes[sexe]
+    estat = int(input("Estat (0=Lleu 1=Greu 2=UCI 3=Mort): ")) #afegir try except per a opcions fora de la llista i de les dates
+    p.estat = estats[estat]
+    iniciStr = input("Inici Contagi (diamesany):  ")
+    p.iniciContagi = datetime.strptime(iniciStr,'%d%m%Y').date()
+    contagiStr = input("Fi Contagi (diamesany) O Enter si està en curs:  ").upper()
+    if contagiStr != "NULL":
+        p.fiContagi = datetime.strptime(contagiStr,'%d%m%Y').date()
+    else:
+        p.fiContagi = "NULL"
+    codi = codi + 1
+    pacients.append(p)
+    cercarPacient(pacients,codi)
 
-#def eliminarPacient(): 
-    
-
-def canviEstatPacient(p:pacient,estat:str):
-    p.estat=estat
+def canviEstatPacient(p:pacient,estat:str): #estic AMB AQEUST
+    estat = int(input("Estat (0=Lleu 1=Greu 2=UCI 3=Mort): ")) #afegir try except per a opcions fora de la llista i de les dates
+    p.estat = estats[estat]
 
 def mostrarPacients(l:list):
+    cap()
     for p in l:
-        print(f"{p.Codi}-{p.dataNaixement}-{p.estat}")
+        printda(p)
 
-def cercarPacient(l:list,codi:str):
+def cercarPacient(l:list,codi):
     for p in l:
-        if p.Codi==codi:
+        if p.codi == codi:
+            cap()
+            printda(p)
             return p
-    return None
 
-codi = 
-opcions = ("Mostrar Pacients","Afegir","Canviar l'estat","Calcular edat","Sortir")
-fi = False
+#Codi per omplenar la llista de pacients aleatoriament
 pacients = []
+codi = 0
+sexes= ["H","D"]
+estats= ["Lleu","Greu","UCI ","Mort"]
+
+inicio = datetime(1967, 1, 1) 
+final = datetime(2003, 12, 31) 
+
+inicio1 = datetime(2021, 1, 1) 
+final1 = datetime(2022, 12, 31) 
+
+#Creacio de pacients
+for i in range(0, 40):
+    datenaix = inicio + (final - inicio) * random.random()
+    datecontagi = inicio1 + (final1 - inicio1) * random.random()
+    p = pacient(codi)
+    p.codi = codi
+    p.dataNaixement = datenaix.date()
+    p.sexe = random.choice(sexes)
+    p.estat = random.choice(estats)
+    p.iniciContagi = datecontagi.date()
+    p.fiContagi = "NULL"
+    codi = codi + 1
+    pacients.append(p) 
+
+opcions = ("Mostrar Pacients","Cercar Pacient","Afegir","Canviar l'estat","Calcular edat","Sortir")
+fi = False
 
 while not fi:
+    input("Presiona enter per continuar")
     clear() 
     for i,opcio in enumerate(opcions):
         print(f"{i+1:2}. {opcio}")
@@ -61,12 +103,13 @@ while not fi:
     opcio = escullOpcio(len(opcions))
     if opcio == "1":
         #mostrar tots els pacients
-        mostrarPacients(pacient)
+        mostrarPacients(pacients)
     elif opcio == "2":
-        #Afegir Pacient"
-        p = nouPacient()
-        pacients.append(p)
+        cercarPacient(pacients,int(input("Introdueix el codi: ")))
     elif opcio == "3":
+        #Afegir Pacient"
+        p = nouPacient(codi)
+    elif opcio == "4":
         #Modificar Pacient
         codi = input("Codi pacient que li vols canviar l'estat: ")
         p = cercarPacient(pacients,codi)
@@ -74,7 +117,11 @@ while not fi:
             #mostrar pacient
             estat = input("Nou Estat: ")
             canviEstatPacient(p,estat)
-    elif opcio == "4":
-        codi = input("Codi pacient que li vols calcular la edat: ")
     elif opcio == "5":
+        codi = input("Codi pacient que li vols calcular la edat: ")
+    elif opcio == "6":
         fi = True
+        
+cap(codi)
+espai = (" "*(5-len(str((p.codi)))))
+print(f"{p.codi}{espai}-   {p.dataNaixement}   -  {p.sexe}   -  {p.estat} -  {p.iniciContagi}   -   {p.fiContagi}")
--- Programació/UF2/Practiques/PR2/Pacient.py
+++ Programació/UF2/Practiques/PR2/Pacient.py
@@ -1,9 +1,11 @@
 from datetime import * 
+import random
 class pacient: 
     dataNaixement:date 
     sexe:str  #H,D 
     estat:str #Lleu,Greu,UCI,mort 
     iniciContagi:date 
     fiContagi:date 
-    def __init__(self,Codi:str): 
-        self.Codi = Codi 
+    def __init__(self,codi:int): 
+        self.codi = codi 
+
