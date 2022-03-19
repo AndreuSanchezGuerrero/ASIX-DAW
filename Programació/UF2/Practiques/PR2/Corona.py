d92023502da899ba88907ecf2b36bc5351877bf7
Tursu <[nil.masso.caba@gmail.com]>
2022-03-19 20:39:55 +0100

    .

--- Programació/UF2/Practiques/PR2/Corona.py
+++ Programació/UF2/Practiques/PR2/Corona.py
@@ -1,10 +1,11 @@
-#el codi autoincrmen tal coincideix amb les altres variables codi, sh'ha de canviar
+#TE = tractaments d'errors que no he tingut temps a fer
+#TE dates incorrectes
 from datetime import *
 import re
 from Pacient import *
 from os import system, name
 
-def escullOpció(n):
+def escullOpcio(n):
     patro = "^\d{1,}$"
     while True:
         o = input("Opció: ")
@@ -39,15 +40,19 @@ def nouPacient(codi):
     estat = int(input("Estat (0=Lleu 1=Greu 2=UCI 3=Mort): ")) #afegir try except per a opcions fora de la llista i de les dates
     p.estat = estats[estat]
     iniciStr = input("Inici Contagi (diamesany):  ")
-    p.iniciContagi = datetime.strptime(iniciStr,'%d%m%Y').date()
-    contagiStr = input("Fi Contagi (diamesany) O Enter si està en curs:  ").upper()
+    iniciC = datetime.strptime(iniciStr,'%d%m%Y').date()
+    while iniciC < p.dataNaixement:
+        iniciStr = input("Inici Contagi (diamesany):  ")
+        iniciC = datetime.strptime(iniciStr,'%d%m%Y').date()
+    p.iniciContagi = iniciC
+    contagiStr = input("Fi Contagi (diamesany) O 'NULL' si està en curs:  ").upper()
     if contagiStr != "NULL":
         p.fiContagi = datetime.strptime(contagiStr,'%d%m%Y').date()
     else:
         p.fiContagi = "NULL"
     codi = codi + 1
     pacients.append(p)
-    cercarPacient(pacients,codi)
+    cercarPacient(pacients,codi,0)
 
 def mostrarPacients(l:list):
     cap()
@@ -55,25 +60,75 @@ def mostrarPacients(l:list):
         printda(p)
 
 def cercarPacient(l:list,codi,opcio):
-    for p in l:
-        if p.Codi == codi:
-            if opcio == 0:
-                cap()
-                printda(p)
-            elif opcio == 1:
-                canviEstatPacient(p)
-            elif opcio == 2:
-                edat(p,input("Data Naixement (diamesany): ")
-    print("Aquest pacient no existeix") #fer q no faci un print per cada pacient no trobat(potser una variable q retorni none?)
+    if codi > len(l)+1:
+        print("Aquest pacient no existeix") 
+    else:
+        n = 0
+        for p in l:
+            if p.Codi == codi:
+                if opcio == 0:
+                    cap()
+                    printda(p)
+                elif opcio == 1:
+                    canviEstatPacient(p)
+                elif opcio == 2:
+                    edat(p,input("Edat a la data (diamesany): "))
+                n = 1
+        if n == 0:
+            print("Aquest pacient no existeix") 
 
 def canviEstatPacient(p:pacient):
-    estat = int(input("Estat (0=Lleu 1=Greu 2=UCI 3=Mort): ")) #afegir try except per a opcions fora de la llista i de les dates
+    estat = int(input("Estat (0=Lleu 1=Greu 2=UCI 3=Mort): ")) #TE opcions fora de la llista
     p.estat = estats[estat]
+    cercarPacient(pacients,p.Codi,0)
 
-def edat(p:pacient,dataStr:date):
+def edat(p:pacient,dataStr:date): #TE per a possibles edats negatives i per a dates mes grans a l'actual canviar el temps del verb
     data = datetime.strptime(dataStr,'%d%m%Y').date()
     edat = data.year - p.dataNaixement.year - ((data.month, data.day) < (p.dataNaixement.month, p.dataNaixement.day))
-    print(f"el pacient {p.Codi} tenia {edat} anys el {data}")
+    if data > date.today():
+        verb = "tindrà"
+    elif data < date.today():
+        verb = "tenia"
+    elif data == date.today():
+        verb = "té"
+    print(f"el pacient {p.Codi} {verb} {edat} anys el {data}")
+
+def pacientsEstat(l:list,estats:list):
+    n=0
+    estat = estats[int(input("Estat (0=Lleu 1=Greu 2=UCI 3=Mort): "))] #TE opcions fora de la llista
+    cap()
+    for p in l:
+        if p.estat == estat:
+            printda(p)
+            n=1
+    if n == 0:
+        clear()
+        print("No hi ha cap pacient en aquest estat")
+
+def pacientsData(l:list):
+    n=0
+    data = datetime.strptime(input("Data Naixement (diamesany): "),'%d%m%Y').date()
+    cap()
+    for p in l:
+        if p.iniciContagi == data:
+            printda(p)
+            n=1
+    if n == 0:
+        clear()
+        print("No hi ha cap pacient contagiat en aquesta data")
+
+def pacientsEstatData(l:list):#Es pot simplificar molt fent us dels altres dos metodes i amb un parametre per escollir entre les 3 opcions
+    n=0
+    estat = estats[int(input("Estat (0=Lleu 1=Greu 2=UCI 3=Mort): "))] #TE opcions fora de la llista
+    data = datetime.strptime(input("Data Naixement (diamesany): "),'%d%m%Y').date()
+    cap()
+    for p in l:
+        if p.iniciContagi == data and p.estat == estat:
+                printda(p)
+                n=1
+    if n == 0:
+        clear()
+        print("No hi ha cap pacient contagiat en aquesta data")
 
 #Codi per omplenar la llista de pacients aleatoriament
 pacients = []
@@ -84,11 +139,11 @@ estats= ["Lleu","Greu","UCI ","Mort"]
 inicio = datetime(1967, 1, 1) 
 final = datetime(2003, 12, 31) 
 
-inicio1 = datetime(2021, 1, 1) 
+inicio1 = datetime(2020, 1, 1) 
 final1 = datetime(2022, 12, 31) 
 
 #Creacio de pacients
-for i in range(0, 40):
+for i in range(0, 40000):
     datenaix = inicio + (final - inicio) * random.random()
     datecontagi = inicio1 + (final1 - inicio1) * random.random()
     p = pacient(codi)
@@ -100,7 +155,7 @@ for i in range(0, 40):
     codi = codi + 1
     pacients.append(p) 
 
-opcions = ("Mostrar Pacients","Cercar Pacient","Afegir","Canviar l'estat","Calcular edat","Sortir")
+opcions = ("Mostrar Pacients","Cercar Pacient","Afegir","Canviar l'estat","Calcular edat","Pacients en estat X","Pacients Contagiats dia X","Pacients en Estat i Data","Sortir")
 fi = False
 
 while not fi:
@@ -124,4 +179,10 @@ while not fi:
     elif opcio == "5":
         cercarPacient(pacients,int(input("Introdueix el codi: ")),2)
     elif opcio == "6":
+        pacientsEstat(pacients,estats) #L'estat no li passem, el demanarà dins
+    elif opcio == "7":
+        pacientsData(pacients)
+    elif opcio == "8":
+        pacientsEstatData(pacients)
+    elif opcio == "9":
         fi = True
