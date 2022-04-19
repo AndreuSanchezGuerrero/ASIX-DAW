e025335859e5d58324c855b58cae1c53a32cbb6b
Tursu <[nil.masso.caba@gmail.com]>
2022-04-19 16:27:55 +0200

    .

--- BDD/MySQL/M02-UF2/Activitat recuperació DML - BBDD Hotels.sql
+++ BDD/MySQL/M02-UF2/Activitat recuperació DML - BBDD Hotels.sql
@@ -49,7 +49,77 @@ WHERE data_inici = data_fi;
 
 SELECT c.client_id, c.nom, c.cognom1
 FROM clients c
-    INNER JOIN reserves r ON (c.client_id = r.client_id)
-    WHERE year(r.data_inici) = 2014 OR year(r.data_inici) = 2015 OR year(r.data_inici) = 2016
-    HAVING count(client_id)
+    INNER JOIN reserves r ON (r.client_id = c.client_id)
+    WHERE year(r.data_inici) = 2014
+        OR year(r.data_inici) = 2015
+        OR year(r.data_inici) = 2016
+    GROUP BY  r.client_id
+    HAVING count(*) > 1
     ORDER BY c.client_id;
+
+/*Quantes reserves va rebre l’hotel ‘Catalonia Ramblas’ de Barcelona durant tot l’any 2015?
+
+    No pots fer servir els Ids de les taules.
+    Una reserva pertany a l'any si la seva data d'inici hi pertany.
+
++--------------+
+| num_reserves |
++--------------+*/
+
+SELECT count(*) as num_reserves
+FROM reserves r
+INNER JOIN habitacions h ON (r.hab_id = h.hab_id)
+INNER JOIN hotels ht ON (h.hotel_id = ht.hotel_id)
+WHERE ht.nom = 'Catalonia Ramblas'
+    AND year(r.data_inici) = 2015;
+
+/*S'han detectat errors a la BD. No pot ser que hi hagin reserves sense cap nit. Busca aquestes reserves i mostra-les
+
+    Mostra l'identificador de la reserva, la data d'inici i la data de fi
+    Ordena el resultat per identificador de la reserva
+
++------------+------------+------------+
+| reserva_id | data_inici | data_fi    |
++------------+------------+------------+*/
+
+SELECT reserva_id, data_inici, data_fi
+FROM reserves
+WHERE data_inici = data_fi
+ORDER BY reserva_id;
+
+/*Quants mesos té la reserva més curta de la BD?
+
+Expressa el resultat en mesos i arrodonint el resultat a 0 decimals
+
++-------+
+| mesos |
++-------+*/
+
+SELECT min(round(DATEDIFF(data_fi, data_inici), 0)) as mesos  
+FROM reserves
+
+/*De l'Hotel 'Catalonia Ramblas' de Barcelona mostra la quantitat de nits disponibles (teòriques) que tindria l'hotel per cada mes de l'any 2016
+
+    Ordena per número de mes de forma ascendent
+
++------+------+
+| mes  | nits |
++------+------+
+|    1 |   ? |
+|    2 |   ? |
+|    3 |   ? |
++------+------+
+...
+|   11 |   ? |
+|   12 |   ? |
+
++------+------+*/
+
+SELECT month(data_inici) as mes, count(*) as nits
+FROM reserves r
+INNER JOIN habitacions h ON (r.hab_id = h.hab_id)
+INNER JOIN hotels ht ON (h.hotel_id = ht.hotel_id)
+WHERE ht.nom = 'Catalonia Ramblas'
+    AND year(r.data_inici) = 2016
+GROUP BY month(data_inici)
+ORDER BY month(data_inici);
