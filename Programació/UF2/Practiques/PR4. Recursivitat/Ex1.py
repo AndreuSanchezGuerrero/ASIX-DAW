3fb2f9ed95ca085c76286e2e3535efb5f687e6e5
Tursu <[nil.masso.caba@gmail.com]>
2022-04-28 17:31:08 +0200

    .

--- Programació/UF2/Practiques/PR4. Recursivitat/Ex1.py
+++ Programació/UF2/Practiques/PR4. Recursivitat/Ex1.py
@@ -13,7 +13,7 @@ for i in range(x):
 def sumar(llista):
     suma = 0
     for i in llista:
-        suma = suma + i
+        suma += i
     return suma
 
 print(sumar(llista))
@@ -35,3 +35,55 @@ def sumarRecursiu2(llista):
         return sumarRecursiu2(llista[:len(llista)//2]) + sumarRecursiu2(llista[len(llista)//2:])
 
 print(sumarRecursiu2(llista))
+
+#4. Fes un mètode al que li passarem la ruta d’una carpeta i ens mostrarà tots els fitxer que hi ha a la ruta passada i a les seves subcarpetes
+def listar(ruta):
+    import os
+    for i in os.listdir(ruta):
+        if os.path.isfile(os.path.join(ruta, i)):
+            print(i)
+        else:
+            listar(os.path.join(ruta, i))
+
+print(listar("."))
+
+#5. Fes un mètode al que li passarem una ruta, i ens dirà la mida total de cada fitxer que hi ha a cada carpeta( mida només dels fitxers de la carpeta) i subcarpetes(recursivament) que hi hagi a la ruta.
+
+
+
+"""6. Ídem anterior, però que a cada carpeta mare li sumi les mides de les subcarpetes filles. Per exemple, si copieu l’estructura del Ondrive de la carpeta Institut, ens haurà de dir el següent:
+
+Institut\Asix1\Cursa de Sant Jordi: 13216 bytes
+
+Institut\Asix1\Sortida 1r Trimestre: 6608 bytes
+
+Institut\Asix1: 39648 bytes
+
+Institut\Asix2\M6: 6641 bytes
+
+Institut\Asix2\M7: 18040 bytes
+
+Institut\Asix2\M8: 57535 bytes
+
+Institut\Asix2: 169768 bytes
+
+Institut: 227459 bytes"""
+
+def mida2(ruta):
+    import os
+    for i in os.listdir(ruta):
+        suma = 0
+        ruta2 = os.path.join(ruta, i)
+        if os.path.isdir(ruta2):
+            
+            print(ruta2, suma)
+
+mida2("./")
+
+#7. Fes un mètode al que li passem una llista d’Alumnes (CodiAlumne,Nom,Cognom 1,Cognom 2) i el codi de l’alumne, ens torni l’alumne corresponent al codi passat.
+from llistaalumnes import *
+def alumne(llista, codi):
+    for i in llista:
+        if i.codialumne == codi:
+            return i
+    return None
--- Programació/UF2/Practiques/PR4. Recursivitat/FileSystem.py
+++ Programació/UF2/Practiques/PR4. Recursivitat/FileSystem.py
@@ -2,8 +2,9 @@ import os
 from datetime import datetime
 from stat import *
 
+
 class Item:
-    def __init__(self,nom,ruta,tipus,dataCreacio,dataModificacio,dataUltimAcces,mida):
+    def __init__(self, nom, ruta, tipus, dataCreacio, dataModificacio, dataUltimAcces, mida):
         self.Nom = nom
         self.Ruta = ruta
         self.Tipus = tipus
@@ -12,14 +13,15 @@ class Item:
         self.DataUltimAcces = dataUltimAcces
         self.Mida = mida
 
-def llistaItems(ruta,filtre):
-    filtre=filtre.upper()
-    llista=[]
-    if filtre in ("F","D"):
+
+def llistaItems(ruta, filtre):
+    filtre = filtre.upper()
+    llista = []
+    if filtre in ("F", "D"):
         for nom in os.listdir(ruta):
             rutaFill = f"{ruta}/{nom}"
-            if (filtre=="F" and os.path.isfile(rutaFill)) or \
-               (filtre=="D" and os.path.isdir(rutaFill)):
+            if (filtre == "F" and os.path.isfile(rutaFill)) or \
+               (filtre == "D" and os.path.isdir(rutaFill)):
                 f = os.stat(rutaFill)
                 nouItem = Item(nom,
                                ruta,
@@ -29,6 +31,5 @@ def llistaItems(ruta,filtre):
                                datetime.fromtimestamp(f.st_atime),
                                f.st_size
                                )
-                llista.append(nouItem)       
+                llista.append(nouItem)
     return llista
-
